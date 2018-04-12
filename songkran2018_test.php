0f5e4e08e20ba918c974977e88d5e398e9e520bc
skttong <sommanuttong@gmail.com>
2018-04-12 22:41:28 +0700

    Add files via upload

---
+++ songkran2018_test.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>My First map</title>
+</head>
+<body>
+    <iframe src="http://164.115.32.133/wwt/testchart/songkran2018.html"  frameborder="0" width="100%" height="100%"></iframe>
+</body>
+</html>
